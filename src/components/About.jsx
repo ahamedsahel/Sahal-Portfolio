004f349a08050e2751083ef1d183600b219ae81a
Ahmed <ahamedsahel@gmail.com>
2026-04-09 10:55:48 +0400

    space change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,6 +7,7 @@ export default function About() {
     ]
 
     return (
+        
         <section className="section" id="about">
             <div className="container">
                 <div className="section-header">
